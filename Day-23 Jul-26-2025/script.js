af715eb3ba0b2260035eb2c32936294738f163de
Asif Abir <88622477+asif-daffodil@users.noreply.github.com>
2025-07-26 17:38:58 +0600

    aasdasda

---
+++ Day-23 Jul-26-2025/script.js
@@ -0,0 +1,102 @@
+var xyz123 = "Hello Woeld";
+var xyz = "Hello World";
+xyz = "Hi there";
+
+let abc = "Hi there";
+abc = "Hi 123";
+
+const xyz = "Assalamuoyalaikum";
+xyz = "Assalamuoyalaikum";
+
+// signle line comment
+
+/*
+multi line
+comment
+*/
+
+// operators
+// Arethmetic operator
+/**
+ * +
+ * -
+ * *
+ * /
+ * %
+ * **
+ */
+
+// Assignment operators
+/**
+ * =
+ * +=
+ * -=
+ * *=
+ * /=
+ * %=
+ * **=
+ */
+
+// increment decrement opearator
+/**
+ * ++
+ * --
+ */
+
+// logical operator
+/**
+ * <
+ * >
+ * <=
+ * >=
+ * ==
+ * !=
+ * ===
+ * !==
+ */
+
+// Conditional opeartor
+/**
+ * &&
+ * ||
+ */
+
+
+// Data Types
+/**
+ * String
+ * number
+ * boolean
+ * undefine
+ * 
+ */
+
+// function
+
+// function myFunc (x = 2, y = 3) {
+//     return x + y;
+// }
+
+// const myFunc = function (x = 2, y = 3) {
+//     return x + y;
+// }
+
+// const myFunc = (x = 2, y = 3) => {
+//     return x + y;
+// }
+
+const myFunc = (x = 2, y = 3) => x + y;
+
+
+myFunc (5, 3);
+myFunc (6, 4);
+myFunc ();
+myFunc (10);
+
+// const newFunc = (x) => {
+//     return x;
+// }
+
+const newFunc = x => x;
+
+newFunc("ha ha ha");
